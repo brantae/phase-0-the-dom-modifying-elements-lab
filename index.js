5b6e3e10ebe2d360622153b7a4f3afed7cd55107
Anslie <brantae@gmail.com>
2022-08-06 03:54:25 -0500

    commit tree f70e9d8200469c2d07a6394f2a0a07598a4d12db

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+
+newHeader.textContent = "Anslie is the champion";
+document.body.append(newHeader);
